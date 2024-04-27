528c4b05389cf084d9409c5b6036a93b965a7853
Azan-10 <justjutt12@gmail.com>
2024-04-27 10:23:06 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+"use strict";
+let user1 = {
+    name: "azan",
+    age: 36,
+    roll: "student"
+};
+let user2 = {
+    name: "aziz",
+    age: 36,
+    roll: "student"
+};
+let user4 = {
+    name: "rauf",
+    age: 14
+};
+console.log(user2);
+console.log(`this  user is belongs to children data type ${user4.name}`);
---
+++ main.ts
@@ -0,0 +1,31 @@
+type user={
+    name:string;
+    age:number;
+    roll:string;}
+type children={
+        name:string,
+        age:number,
+    }
+
+    let user1:user={
+    name:"azan",
+    age:36,
+    roll:"student"
+
+}
+    let user2:user={
+        name:"aziz",
+        age:36,
+        roll:"student"
+    
+    }
+    
+        let user4:children={
+            name:"rauf",
+            age:14
+    }
+
+
+
+console.log(user2)
+console.log(user4)
